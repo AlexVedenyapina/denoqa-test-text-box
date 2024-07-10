29816b1c29679612d079b46a81110c53bd7d8e9e
AlexVedenyapina <sahynkia@gmal.com>
2024-07-10 16:39:41 +0300

    added Javaclass testTextBox and added dependency to the build.gradle

---
+++ src/test/java/testTextBox.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class testTextBox {
+
+    @BeforeAll
+    static void beforAll(){
+        Configuration.browserSize ="1920x1080";
+        Configuration.baseUrl= "https://demoqa.com";
+    }
+
+    @Test
+    void fileFromTest(){
+        open("/text-box");
+        $("#userName").setValue("Svetlana Kirchenko");
+        $("#userEmail").setValue("svetlana@kirchenko.ru");
+        $("#currentAddress").setValue("Stavropol, Mira 123");
+        $("#permanentAddress").setValue("Address 2");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Svetlana Kirchenko"), text("svetlana@kirchenko.ru"),
+                text("Stavropol, Mira 123"), text("Address 2"));
+
+    }
+
+}
